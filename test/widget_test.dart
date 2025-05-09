9fe31852d4c01cd6cb1b586b011fcb54a5717721
ghiyaazt <redgmaing66@gmail.com>
2025-05-09 10:16:47 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pintar_hidroponik/main.dart';
+import 'package:hidroponiktkkc/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
